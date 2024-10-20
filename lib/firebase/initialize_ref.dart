e51d9c1a0a61b44989c4a0cb5d30941225960404
Abu-Nabe <abubacarnabe@gmail.com>
2024-10-20 21:58:59 +0700

    db issue

---
+++ lib/firebase/initialize_ref.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<DatabaseReference> setUpFirebaseRef() async {
+  // Load the .env file
+  await dotenv.load(fileName: ".env");
+
+  // Create a DatabaseReference using the URL from the .env file
+  DatabaseReference dbRef = FirebaseDatabase.instance.refFromURL(
+      dotenv.env['FIREBASE_REAL_TIME_DATABASE_ID'] ?? ''
+  );
+  // Return the DatabaseReference
+  return dbRef;
+}
