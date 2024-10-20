ad1b077a16b26a0f6f8b7fa9769028577b894304
Abu-Nabe <abubacarnabe@gmail.com>
2024-10-20 21:42:26 +0700

    registration functionalities

--- lib/firebase/authenticated_state.dart
+++ lib/firebase/authenticated_state.dart
@@ -10,18 +10,23 @@ class AuthWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.authStateChanges(), // Listen for auth state changes
+    return FutureBuilder<User?>(
+      future: _getCurrentUser(), // Get the current user once
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(child: CircularProgressIndicator()); // Show a loading spinner while waiting
         }
-        if (snapshot.hasData) {
+        if (snapshot.hasData && snapshot.data != null) {
           return const BottomNavigation(); // User is signed in, show bottom navigation
         } else {
           return const LoginController(); // User is not signed in, show login page
         }
       },
     );
+  }
+
+  // Function to get the current user
+  Future<User?> _getCurrentUser() async {
+    return FirebaseAuth.instance.currentUser; // Get the current user
   }
 }
--- lib/firebase/create_account.dart
+++ lib/firebase/create_account.dart
@@ -1,5 +1,8 @@
 import 'package:aag_group_services/consts/strings/authentication_strings/authentication_strings.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+import '../authentication_pages/functions/update_auth.dart';
 
 Future<UserCredential?> createAnAccount(FirebaseAuth auth, Map<String, dynamic> authDetails) async {
   try {
@@ -13,15 +16,33 @@ Future<UserCredential?> createAnAccount(FirebaseAuth auth, Map<String, dynamic> 
   } on FirebaseAuthException catch (e) {
     // Handle errors here
     if (e.code == 'weak-password') {
-      authDetails[registerError] = "The password provided is too weak.";
+      updateAuthDetails(authDetails, registerError, 'The password provided is too weak.');
     } else if (e.code == 'email-already-in-use') {
-      authDetails[registerError] = "The account already exists for that email.";
+      updateAuthDetails(authDetails, registerError, 'The account already exists for that email.');
     } else {
-      authDetails[registerError] = "Error Creating An Account.";
+      updateAuthDetails(authDetails, registerError, 'Error Creating An Account.');
     }
     return null; // Return null on error
   } catch (e) {
-    authDetails[registerError] = "Error Creating An Account.";
+    updateAuthDetails(authDetails, registerError, 'Error Creating An Account.');
     return null; // Return null for any other error
+  }
+}
+
+Future<bool> addAccountToDb(String userId, Map<String, dynamic> authDetails) async {
+  final DatabaseReference _database = FirebaseDatabase.instance.ref();
+
+  String fullname = '${authDetails['registerFirstName']} ${authDetails['registerLastName']}';
+  try {
+    await _database.child('users/$userId').set({
+      'name': fullname,
+      'email': authDetails[registerEmail],
+      'created_at': ServerValue.timestamp, // Optional: for creation timestamp
+    });
+    return true;
+  } catch (e) {
+    updateAuthDetails(authDetails, registerError, 'Error Creating An Account.');
+    print(e.toString());
+    return false;
   }
 }
