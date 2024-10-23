a3b2695e1e6c4650f3bb0e11b91d2cc086086a64
Abu-Nabe <abubacarnabe@gmail.com>
2024-10-23 23:12:18 +0700

    fixing retrieve users error

--- lib/main_pages/controllers/contact.dart
+++ lib/main_pages/controllers/contact.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
+import '../../authentication_pages/models/user_model.dart';
 import '../screens/contact_screen.dart';
 
 class ContactsPage extends StatefulWidget {
@@ -10,14 +12,51 @@ class ContactsPage extends StatefulWidget {
 }
 
 class ContactsPageState extends State<ContactsPage> {
+  ValueNotifier<List<UserModel>> usersList = ValueNotifier<List<UserModel>>([]);
+  final database = FirebaseDatabase.instance.ref();
 
   @override
   void initState() {
     super.initState();
+
+    usersList.addListener(updateValue);
+
+    fetchUsers();
+  }
+
+  void updateValue(){
+    setState(() {});
+  }
+
+  void fetchUsers() {
+    // Listening for changes in the 'users' node
+    database.child('users').onValue.listen((event) {
+      DataSnapshot snapshot = event.snapshot;
+
+      if (snapshot.exists) {
+        List<UserModel> fetchedUsers = [];
+        Map<dynamic, dynamic> usersMap = snapshot.value as Map<dynamic, dynamic>;
+
+        // Iterate over each user ID and map each user data to UserModel
+        usersMap.forEach((userId, userData) {
+          // Make sure to handle the case where userData might not be a Map
+          if (userData is Map) {
+            fetchedUsers.add(UserModel.fromMap(Map<String, dynamic>.from(userData)));
+          }
+        });
+
+        // Update the state with the new user list
+        usersList.value = fetchedUsers; // This assumes usersList is a List<UserModel>
+        // print(fetchedUsers.length);
+      }
+    }, onError: (error) {
+      print('Error listening to users: $error');
+    });
   }
 
   @override
   void dispose() {
+    usersList.dispose(); // Dispose the ValueNotifier
     super.dispose();
   }
 
