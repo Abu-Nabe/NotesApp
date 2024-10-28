9be9ee65bb5acbcaa3ef91d240c4bdc1a060d81b
Abu-Nabe <abubacarnabe@gmail.com>
2024-10-29 00:08:25 +0700

    friends complete

--- lib/design/main_pages/functions/contact_functions/check_friends.dart
+++ lib/design/main_pages/functions/contact_functions/check_friends.dart
@@ -2,9 +2,8 @@ import 'package:aag_group_services/design/authentication_pages/models/user_model
 import 'package:aag_group_services/design/main_pages/controllers/contact.dart';
 
 bool checkFriends(UserModel searchedUser) {
-  // Get the list of current user's friends
-  List<UserModel> friendsList = ContactsPageState.usersList.value; // Assuming this is a list of UserModel
+  List<UserModel> friendsList = ContactsPageState.usersList.value;
 
-  // Check if the searched user's ID is in the friends list
-  return friendsList.any((friend) => friend.id != searchedUser.id);
+  // Ensure the IDs are compared as strings to prevent type mismatch
+  return !friendsList.any((friend) => friend.id.toString() == searchedUser.id.toString());
 }
