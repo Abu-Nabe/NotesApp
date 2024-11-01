0a02e4c4542bc111989a7f3b2e266df858f9d27e
abu <abu.nabe@raytail.com>
2024-11-01 22:42:13 +0700

    message container

--- lib/design/communications/screens/message_screen.dart
+++ lib/design/communications/screens/message_screen.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/cupertino.dart';
+
 import 'package:aag_group_services/consts/colors.dart';
 import 'package:aag_group_services/design/authentication_pages/models/user_model.dart';
 import 'package:aag_group_services/design/communications/controllers/notes_controller.dart';
@@ -9,9 +11,8 @@ import 'package:flutter/material.dart';
 import '../../../extension/convert_time.dart';
 import '../../../firebase/user_info.dart';
 import '../../const/reusable_layouts/toolbar_shadow_line.dart';
-import '../functions/add_note_firebase.dart';
 
-Widget build_notes_screen(BuildContext context, UserModel receiverModel) {
+Widget build_message_screen(BuildContext context, UserModel receiverModel) {
   final size = MediaQuery.of(context).size;
 
   return Scaffold(
@@ -41,12 +42,11 @@ Widget buildToolbar(BuildContext context, String name) {
     leading: IconButton(
       icon: Icon(Icons.arrow_back, color: Colors.black), // Back arrow icon
       onPressed: () {
-        NotesControllerState.notesList.value = [];
         Navigator.pop(context); // Navigates back to the previous screen
       },
     ),
     title: Text(
-      "${name}'s Notes", // Title of the toolbar with string interpolation
+      "${name}", // Title of the toolbar with string interpolation
       style: TextStyle(
         color: Colors.black, // Title text color
         fontWeight: FontWeight.bold, // Bold text
@@ -70,65 +70,68 @@ Widget buildNoteList(BuildContext context) {
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8), // Padding around the list
         itemCount: notes.length,
         itemBuilder: (context, index) {
-          // Mini texts displayed as individual notes
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4), // Space between each note
-            child: Align(
-              alignment: Alignment.centerLeft, // Align notes to the left
-              child: Container(
-                padding: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(8), // Rounded corners
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0, 3), // Position of shadow
-                    ),
-                  ],
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start, // Align texts to the start
-                  children: [
-                    Text(
-                      notes[index].message, // First text: the note itself
-                      style: TextStyle(
-                        fontSize: 14, // Mini text size
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold, // Makes the note bold
-                      ),
-                    ),
-                    SizedBox(height: 4), // Add some space between the note and the sender
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          notes[index].name, // Second text: the sender
-                          style: TextStyle(
-                            fontSize: 12, // Smaller text size for the sender
-                            color: Colors.grey[700], // A lighter color for the sender
-                          ),
-                          overflow: TextOverflow.ellipsis, // Add ellipsis if text overflows
-                          maxLines: 1, // Restrict to a single line
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          formatDateTime(notes[index].createdAt.toString()), // Second text: the sender
-                          style: TextStyle(
-                            fontSize: 12, // Smaller text size for the sender
-                            color: Colors.grey[700], // A lighter color for the sender
-                          ),
-                        ),
-                      ]
-                    )
-                  ],
-                ),
+          return buildMessageLeftContainer(notes[index]);
+        },
+      ),
+    ),
+  );
+}
+
+Widget buildMessageLeftContainer(NotesModel note){
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 4), // Space between each note
+    child: Align(
+      alignment: Alignment.centerLeft, // Align notes to the left
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8), // Rounded corners
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3), // Position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start, // Align texts to the start
+          children: [
+            Text(
+              note.message, // First text: the note itself
+              style: TextStyle(
+                fontSize: 14, // Mini text size
+                color: Colors.black,
+                fontWeight: FontWeight.bold, // Makes the note bold
               ),
             ),
-          );
-        },
+            SizedBox(height: 4), // Add some space between the note and the sender
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    note.name, // Second text: the sender
+                    style: TextStyle(
+                      fontSize: 12, // Smaller text size for the sender
+                      color: Colors.grey[700], // A lighter color for the sender
+                    ),
+                    overflow: TextOverflow.ellipsis, // Add ellipsis if text overflows
+                    maxLines: 1, // Restrict to a single line
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    formatDateTime(note.createdAt.toString()), // Second text: the sender
+                    style: TextStyle(
+                      fontSize: 12, // Smaller text size for the sender
+                      color: Colors.grey[700], // A lighter color for the sender
+                    ),
+                  ),
+                ]
+            )
+          ],
+        ),
       ),
     ),
   );
@@ -141,7 +144,7 @@ Widget buildTextField(BuildContext context, String sender, String receiver) {
     child: TextField(
       controller: NotesControllerState.noteController.value,
       decoration: InputDecoration(
-        hintText: 'Write a note...',
+        hintText: 'Send a message',
         contentPadding: EdgeInsets.only(left: 16), // Adjust left padding
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
@@ -155,14 +158,7 @@ Widget buildTextField(BuildContext context, String sender, String receiver) {
           icon: Icon(Icons.send),
           color: ShadesOfPurple.purple_iris, // Set icon color
           onPressed: () async {
-            Map<String, String> userInfo = await fetchUserInfo();
 
-            // Handle send action
-            String message = NotesControllerState.noteController.value.text;
-            addNoteToDB(sender, receiver, message, userInfo['username'].toString());
-            addFriendNoteToDB(sender, receiver, message, userInfo['username'].toString());
-
-            NotesControllerState.noteController.value.clear();
           },
         ),
       ),
