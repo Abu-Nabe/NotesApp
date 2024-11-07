4f505b2a50ee46d96cacd9a21aeb4299db1864ac
Abu-Nabe <abubacarnabe@gmail.com>
2024-11-07 22:18:18 +0700

    settings view

--- lib/design/main_pages/controllers/settings.dart
+++ lib/design/main_pages/controllers/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../firebase/sign_out.dart';
+import '../../../firebase/user_info.dart';
 import '../screens/settings_screen.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -10,16 +11,38 @@ class SettingsPage extends StatefulWidget {
 }
 
 class SettingsPageState extends State<SettingsPage> {
-  // Any logic or state specific to Settings Page
+
+  static ValueNotifier<Map<String, String>> userInfo = ValueNotifier<Map<String, String>>({});
 
   @override
   void initState() {
     super.initState();
-    signOut();
+    // Initialization code here, e.g., setting up listeners or fetching data
+    userInfo.addListener(updateValue);
+
+    configUser();
+  }
+
+  Future<void> configUser() async {
+    userInfo.value = await fetchUserInfo();
+  }
+
+  void updateValue(){
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    userInfo.removeListener(updateValue);
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return build_settings_screen(context);
   }
+
 }
+
+
